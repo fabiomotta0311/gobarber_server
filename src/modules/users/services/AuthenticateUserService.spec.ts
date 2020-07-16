188859a171463abd7a6fa49f9cff29bc9dd44fed
Fabio Motta <fabiomotta0311@gmail.com>
2020-07-16 02:35:53 -0300

    auth tests done

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -32,6 +32,7 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
   it('should not be able to authenticate with non existing user', async () => {
     const fakeUserRepository = new FakeUserRepository();
     const fakeHashProvider = new FakeHashProvider();
@@ -48,6 +49,7 @@ describe('AuthenticateUser', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
   it('should not be able to authenticate with wrong password', async () => {
     const fakeUserRepository = new FakeUserRepository();
     const fakeHashProvider = new FakeHashProvider();
