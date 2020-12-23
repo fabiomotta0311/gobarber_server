657441b600cb72c6358f921f7e7dfe3de40411c2
Fabio Motta <fabiomotta0311@gmail.com>
2020-12-23 05:07:10 -0300

    emails templates

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,4 +1,3 @@
-// import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
 import { container } from 'tsyringe';
 
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
@@ -6,6 +5,9 @@ import DiskStorageProvider from '@shared/container/providers/StorageProvider/imp
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -15,4 +17,9 @@ container.registerSingleton<IStorageProvider>(
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
 );
