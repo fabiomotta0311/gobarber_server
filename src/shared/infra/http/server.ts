954056c881ac581490119d4a80af1231b4405baf
Fabio Motta <fabiomotta0311@gmail.com>
2020-12-29 23:04:33 -0300

    Route validations

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppErrors';
 import routes from '@shared/infra/http/routes';
@@ -16,6 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(errors());
+
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     // Erro conhecido - Gerado pela minha aplicação
